2bb84f41831247c1e61726a1d79a948d7bdf2f5c
Hunter Raffety <hsraffety@gmail.com>
2019-10-08 07:43:42 -0700

    selection sort complete

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,21 +6,26 @@ def selection_sort( arr ):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
+        print(f"current index {cur_index}")
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # print(arr[cur_index])
         for j in range(cur_index, len(arr)):
-            print(f"this is arr {arr}, this is j: {arr[j]}, this is the current index: {cur_index}")
-
-
+            print(arr)
+            # print(f"this is arr {arr}, this is j: {arr[j]}, this is the current index: {cur_index}")
+            if arr[j] < arr[smallest_index]: # if this current item in this NEW loop is less than what I previously had stored up in that smallest index for the array. Measured against itself. This is why the swap is so important so that I can move them around after each iteration through the array but won't lose track of what is what
+                smallest_index = j
+                # print(smallest_index, j)
         # TO-DO: swap
-
+        temp = arr[smallest_index] #arr[smallest_index] is the placeholder for when cur_index is being assigned to that value as the new smallest index, so that the current_index can be assigned to the temporary value.
+        arr[smallest_index] = arr[cur_index]
+        arr[cur_index] = temp
 
 
 
     return arr
 
-selection_sort(arr)
+print(selection_sort(arr))
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
