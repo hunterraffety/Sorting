574b657079fed7b60a539defee0aad5486d965a1
Hunter Raffety <hsraffety@gmail.com>
2019-10-08 09:08:17 -0700

    working on bubble sort but lecture starting.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,12 +6,12 @@ def selection_sort( arr ):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        print(f"current index {cur_index}")
+        # print(f"current index {cur_index}")
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # print(arr[cur_index])
         for j in range(cur_index, len(arr)):
-            print(arr)
+            # print(arr)
             # print(f"this is arr {arr}, this is j: {arr[j]}, this is the current index: {cur_index}")
             if arr[j] < arr[smallest_index]: # if this current item in this NEW loop is less than what I previously had stored up in that smallest index for the array. Measured against itself. This is why the swap is so important so that I can move them around after each iteration through the array but won't lose track of what is what
                 smallest_index = j
@@ -27,12 +27,41 @@ def selection_sort( arr ):
 
 print(selection_sort(arr))
 
+arr = [1, 3, 5, 7, 10, 6, 2, 0, 4, 8] #reset my array
+
+    # Keywords that imply a loop is required
+    #     while / for
+    #     as long as
+    #     until
+    # Conditions that can be checked with if/else blocks
+    #     when we find __
+    #     if __ happens
+    #     once this value is large/smaller
+    # What type of value should be returned?
+
+
+# def bubble_sort(elements):  # If `elements` is a collection, remember it will be passed by reference, not value
+
+  # We only need to loop through `elements` until we make a pass that leads to 0 swaps. How do we keep track of whether or not any swaps have occurred? #create a swap variable to keep track
+  # Do we always need to loop through all elements?
+    # Depending on how our loop was set up, which neighbors should be compared?
+    # Can we do swaps in Python without a `temp` variable?
+    # only the guys on the right hand side matter for what we're looking at in a bubble sort
+
+  # What, if anything needs to be returned?
+  # the array
+
+
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    swap = True
+    while swap:
+        swap = False
+        for i in range(0, len(arr) - 1):
+            print(f"index: {i}, array item in that index: {arr[i]}")
     return arr
 
-
+print(bubble_sort(arr))
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
 
