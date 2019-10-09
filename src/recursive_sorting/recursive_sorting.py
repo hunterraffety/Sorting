73f77c6279aa85e7e5565f83b07122b65f681250
Hunter Raffety <hsraffety@gmail.com>
2019-10-09 09:45:24 -0700

    final commit

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,4 +1,5 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
+'''
 arrA = [1, 2, 3, 4]
 arrB = [5, 6, 7, 8]
 
@@ -28,22 +29,22 @@ def merge( arrA, arrB ):
 
     for i in range(elements):
         if i >= len(arrA):  # this guy is empty
-            print(arrB[j], merged_arr[i])
+            #print(arrB[j], merged_arr[i])
             merged_arr[i] = arrB[j]
             j += 1 # ends
         elif i >= len(arrB):  # this guy is empty
-            print(arrA[i], merged_arr[i])
+            #print(arrA[i], merged_arr[i])
             merged_arr[i] = arrA[i]
             i =+ 1 # ends
     # while i < len(arrA) and j < len(arrB):
         # current = i
         elif arrA[i] < arrB[j]: # element at this location is smaller
-            print(arrA[i], merged_arr[i])
+            #print(arrA[i], merged_arr[i])
             merged_arr[i] = arrA[i]
             # merged_arr.append(arrA[i]) #appends item, but at the end, doesn't overwrite the zeros that were populated based on the elements
             i += 1 # increment the index item
         else: # element at arrB is smaller
-            print(arrB[j], merged_arr[i])
+            #print(arrB[j], merged_arr[i])
             merged_arr[i] = arrB[j]
             # merged_arr.append(arrB[j])
             j += 1
@@ -64,13 +65,66 @@ def merge( arrA, arrB ):
 print(merge(arrA, arrB))
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
+# def merge_sort( arr ):
     # TO-DO
     # need to check the array is large enough
     # need to split arr into two. // by 2
     # need to define which sides are which by recursively calling merge_sort
     # need to join with merge helper function
-    return arr
+    # return arr
+
+
+arr = [1, 3, 5, 7, 2, 4, 6, 8]
+'''
+# solution from lecture 10.09.19
+
+def merge(arrA, arrB):
+    #merge step takes two sorted arrays, merge them to a single sorted array containing all elements from both arrays
+    #create a new array, length of lenArrA, lenArrB
+    elements = len(arrA) + len(arrB)
+    merged_arr = [0] * elements # avoids shifting and copying, efficiency
+    #create markers for a and b starting at 0
+    a = 0
+    b = 0
+    #while a and b are less than the length of arrA and arrB respectively
+    #compare the items at indices a/b, add smallest to merged array
+    #increment a or b, depending on which was smallest
+    for i in range(0, elements):
+        if a >= len(arrA):
+            merged_arr[i] = arrB[b]
+            b += 1
+        elif b >= len(arrB):
+            merged_arr[i] = arrA[a]
+            a += 1
+        elif arrA[a] < arrB[b]:
+            merged_arr[i] = arrA[a]
+            a += 1
+        else: # arrA[a] >= arrB[b]:
+            merged_arr[i] = arrB[b]
+            b += 1
+    print(f"MERGED: {merged_arr}")
+    return merged_arr
+
+
+
+arr = [1, 3, 5, 7, 2, 4, 6, 8, 3]
+def merge_sort(arr):
+    print(f"merge sort: {arr}")
+    #take in unsorted lists
+    #return a sorted list
+    if len(arr) <= 1: # Base Case
+        return arr  # Base Case
+    else:
+        #split this in half, sort the halves
+        left_half =  arr[0 : len(arr) // 2] #splices first half off #something
+        right_half =  arr[len(arr) // 2 : ] #this one splices last half off #something
+        #merge the sorted halves
+        left_sorted = merge_sort(left_half)
+        right_sorted = merge_sort(right_half)
+        return merge(left_sorted, right_sorted)
+
+merge_sort(arr)
+
 
 
 # STRETCH: implement an in-place merge sort algorithm
